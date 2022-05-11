ac14462120af19e28c4456372d6f0353ed160af0
Victor <nascimentovictor22@gmail.com>
2022-05-10 21:31:38 -0300

    Calculadora com Retorno

---
+++ CalculadoraFuncoesComRetorno/CalculadoraFuncoesComRetorno/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace CalculadoraFuncoesComRetorno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int respostaSoma;
+            int respostaSubtracao;
+            int respostaMultiplicacao;
+            int respostaDivisao;
+            int opcao;
+
+
+            do
+            {
+
+                Console.Write("1 - Soma 2 - Subtração 3 - Divisão 4 - Multiplicacao 0 - Finalizar Programa\n");
+                Console.Write("Digite uma opcao : ");
+                opcao = Convert.ToInt16(Console.ReadLine()); 
+
+
+                switch (opcao)
+                {
+                    case 1:
+                        {
+                       
+                            respostaSoma = Soma(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+                            
+                            Console.WriteLine($"Resposta Soma : {respostaSoma}");
+                        }
+                        break;
+
+                    case 2:
+                        {
+                            respostaSubtracao = subtracao(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+                            Console.WriteLine($"Resposta Subtração : {respostaSubtracao}");
+                        }
+                        break;
+
+                    case 3:
+                        {
+                            respostaDivisao = divisao(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+                            Console.WriteLine($"Resposta Divisao : {respostaDivisao}");
+                        }
+                        break;
+
+                    case 4: 
+                        {
+                            respostaMultiplicacao = multiplicacao(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+                            Console.WriteLine($"Resposta Multiplicacao : {respostaMultiplicacao}");
+                        }
+                        break;
+                }
+            } while (opcao != 0);                    
+        }
+
+        static int Soma(int a, int b)
+        {    
+            return a + b;
+
+        }
+
+        static int subtracao(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int divisao (int a, int b)
+        {
+
+            if (b == 0)
+            {
+                Console.WriteLine("Não é possivel dividir um valor por 0");
+            }
+            return a / b;
+
+        }
+
+        static int multiplicacao (int a, int b)
+        {
+
+            return a * b;
+
+        }
+
+
+    }
+}
